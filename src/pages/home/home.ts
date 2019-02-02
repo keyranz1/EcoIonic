742cb775b2ecb2ee3f5e6517da01522c7838b444
Kiran Yadubanshi <kiran.yadubanshi@selu.edu>
2019-02-01 19:21:02 -0600

    product listing on homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, Slides } from 'ionic-angular';
+import { NavController, Slides, ToastController } from 'ionic-angular';
 
 import * as WooCommerce from 'woocommerce-api';
 
@@ -11,18 +11,24 @@ import * as WooCommerce from 'woocommerce-api';
 export class HomePage {
 
   wooCommerce: any;
-  products = [];
+  products: any[];
+  moreProducts: any[];
+  page : number;
 
   @ViewChild('itemSlides') itemSlides: Slides;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
+    this.page = 2;
+
     this.wooCommerce = WooCommerce({
-      url: "http://localhost:8888/myServer/",
-      consumerKey: "ck_b85fb8ebe54ccec41de71055905281025fc12f11",
-      consumerSecret: "cs_8e02bf6198df63c05143d725e791aeb120808ace"
+      url: 'http://localhost:8888/myServer/',
+      consumerKey: 'ck_b85fb8ebe54ccec41de71055905281025fc12f11',
+      consumerSecret: 'cs_8e02bf6198df63c05143d725e791aeb120808ace'
     });
 
-    this.wooCommerce.getAsync("products").then((data) => {
+    this.loadMoreProducts(null);
+
+    this.wooCommerce.getAsync('products').then((data) => {
       console.log(JSON.parse(data.body));
       this.products = JSON.parse(data.body).products;
     }, (err) => {
@@ -35,7 +41,34 @@ export class HomePage {
       if(this.itemSlides.getActiveIndex() == this.itemSlides.length() -1) {
         this.itemSlides.slideTo(0);
       }
-      this.itemSlides.slideNext()
-    },3500)
+      this.itemSlides.slideNext();
+    },3500);
+  }
+
+  loadMoreProducts(event){
+    if(event == null){
+      this.page = 1;
+      this.moreProducts = [];
+    } else {
+      this.page ++;
+    }
+
+    this.wooCommerce.getAsync('products?page='+this.page).then((data) => {
+      console.log(JSON.parse(data.body));
+      this.moreProducts = this.moreProducts.concat(JSON.parse(data.body).products);
+      if(event != null){
+        event.complete();
+      }
+
+      if(JSON.parse(data.body).products.length < 10){
+        event.enable(false);
+        this.toastCtrl.create({
+          message: 'No more new products!',
+          duration:1500
+        }).present();
+      }
+    }, (err) => {
+      console.log(err);
+    });
   }
 }
