9c6a8f9656df8b148b6a297b026eeb52894a8081
Kiran Yadubanshi <kiran.yadubanshi@selu.edu>
2019-02-02 03:53:30 -0600

    product category for menu

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage} from '../home/home';
-
+import * as WooCommerce from 'woocommerce-api';
 /**
  * Generated class for the MenuPage page.
  *
@@ -16,9 +16,30 @@ import { HomePage} from '../home/home';
 export class MenuPage {
 
   homePage: any;
+  wooCommerce: any;
+  categories: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.homePage = HomePage
+    this.homePage = HomePage;
+    this.categories = [];
+    this.wooCommerce = WooCommerce({
+      url: 'http://localhost:8888/myServer/',
+      consumerKey: 'ck_b85fb8ebe54ccec41de71055905281025fc12f11',
+      consumerSecret: 'cs_8e02bf6198df63c05143d725e791aeb120808ace'
+    });
+
+    this.wooCommerce.getAsync("products/categories").then((data)=>{
+      console.log(JSON.parse(data.body).product_categories);
+      let temp: any[] = JSON.parse(data.body).product_categories;
+
+      for(let i = 1; i< temp.length; i++ ){
+        if(temp[i].parent == 0) {
+          this.categories.push(temp[i]);
+        }
+      }
+    },(err)=>{
+      console.log(err);
+    })
   }
 
   ionViewDidLoad() {
